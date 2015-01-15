334b8e37ef5322ff7ad619ea0a01a89b4bef05db
Kostas Antonopoulos <kostas@devpro.gr>
2015-01-15 20:25:13 +0200

    Added and implemented DPUserDefaults

---
+++ DPUserDefaults/DPUserDefaults/DPUserDefaults.h
@@ -0,0 +1,28 @@
+//
+//  DPUserDefaults.h
+//  DPUserDefaults
+//
+//  Created by Kostas on 15/1/15.
+//  Copyright (c) 2015 Kostas Antonopoulos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DPUserDefaults : NSObject
+
++(DPUserDefaults*)sharedUserDefaults;
+
+-(void)setUserId:(NSString*)userId;
+-(NSString*)currentUserId;
+
+
+- (id)objectForKey:(NSString *)key;
+- (void)setObject:(id)value forKey:(NSString *)key;
+- (void)removeObjectForKey:(NSString *)key;
+
+- (void)setBool:(BOOL)value forKey:(NSString *)key;
+- (BOOL)boolForKey:(NSString *)key;
+
+- (BOOL)synchronize;
+
+@end
---
+++ DPUserDefaults/DPUserDefaults/DPUserDefaults.m
@@ -0,0 +1,72 @@
+//
+//  DPUserDefaults.m
+//  DPUserDefaults
+//
+//  Created by Kostas on 15/1/15.
+//  Copyright (c) 2015 Kostas Antonopoulos. All rights reserved.
+//
+
+#import "DPUserDefaults.h"
+
+@interface DPUserDefaults (){
+    NSString *_userId;
+}
+
+@end
+
+@implementation DPUserDefaults
+
+static DPUserDefaults *shared;
+
++(DPUserDefaults*)sharedUserDefaults{
+    if (!shared) {
+        shared = [[DPUserDefaults alloc]init];
+    }
+    return shared;
+}
+
+#pragma mark - User Methods
+
+-(void)setUserId:(NSString*)userId{
+    _userId = userId;
+}
+
+-(NSString*)currentUserId{
+    return _userId;
+}
+
+-(NSString*)userKeyForKey:(NSString*)key{
+    return [NSString stringWithFormat:@"DPUserDefaults-%@-%@",_userId,key];
+}
+
+#pragma mark - Object
+
+- (id)objectForKey:(NSString *)key{
+    return [[NSUserDefaults standardUserDefaults]objectForKey:[self userKeyForKey:key]];
+}
+
+- (void)setObject:(id)value forKey:(NSString *)key{
+    [[NSUserDefaults standardUserDefaults] setObject:value forKey:[self userKeyForKey:key]];
+}
+
+- (void)removeObjectForKey:(NSString *)key{
+    [[NSUserDefaults standardUserDefaults]removeObjectForKey:[self userKeyForKey:key]];
+}
+
+#pragma mark - Bool
+
+- (void)setBool:(BOOL)value forKey:(NSString *)key{
+    [[NSUserDefaults standardUserDefaults]setBool:value forKey:[self userKeyForKey:key]];
+}
+
+- (BOOL)boolForKey:(NSString *)key{
+    return [[NSUserDefaults standardUserDefaults]boolForKey:[self userKeyForKey:key]];
+}
+
+#pragma mark - Synchronize
+
+- (BOOL)synchronize{
+    return [[NSUserDefaults standardUserDefaults]synchronize];
+}
+
+@end
