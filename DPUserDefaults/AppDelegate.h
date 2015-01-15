899e92a3b3829d44320aabda3d21a3b286666d96
Kostas Antonopoulos <kostas@devpro.gr>
2015-01-15 20:07:14 +0200

    Initial Commit

---
+++ DPUserDefaults/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DPUserDefaults
+//
+//  Created by Kostas on 15/1/15.
+//  Copyright (c) 2015 Kostas Antonopoulos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DPUserDefaults/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DPUserDefaults
+//
+//  Created by Kostas on 15/1/15.
+//  Copyright (c) 2015 Kostas Antonopoulos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
